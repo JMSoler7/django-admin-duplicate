ab12c5e3f0ac9df097856ed83083545e4fdcd9bc
Joan Marc Soler <joanmarc@calidae.com>
2019-10-29 20:21:56 +0100

    Use record instead object reserved variable

--- admin-duplicate/models.py
+++ admin-duplicate/models.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 class DuplicateAdminMixin(admin.ModelAdmin):
 
     def duplicate_event(self, request, queryset):
-        for object in queryset:
-            object.id = None
-            object.save()
+        for record in queryset:
+            record.id = None
+            record.save()
 
     actions = [duplicate_event]
     duplicate_event.short_description = "Duplicate selected items"
